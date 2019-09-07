c027dd52bfa5a9cfea2b3ec2323ee5d39229a97d
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-07 13:27:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,15 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
-	## code here
-  end
+    new_hash = data[value].values.flatten.uniq
+     
+
+    
+#    value.each do |attributes, array|
+#      array.each do |names|
+        
+#        if 
+end
   new_hash
 #  binding_pry
 end
