a4b4ce3a0dd21ad495374785c54b40f7a02ec235
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-07 12:59:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,58 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  name_hash = {}
+  data.each do |attributes, values|
+    values.each do |value, array|
+      array.each do |name|
+
+        if name_hash[name] == nil
+          name_hash[name] = {}
+          name_hash[name][attributes] = []
+
+        else
+          name_hash[name][attributes]
+
+
+        end
+      end
+    end
+  end
+
+name_hash.each do |name, values|
+  values.each do |hashvalue, array|
+    data.each do |attributes, values|
+      values.each do |value, array|
+        array.each do |element|
+
+          if element == name && hashvalue && attributes
+            name_hash[name][hashvalue] << values
+          end
+        end
+      end
+    end
+  end
 end
+name_hash
+binding.pry
+end
+
+
+#problem: when I pull up name_hash, it is structured correctly to start then includes names in an array under each attribute:
+
+# need a conditional to check whether a name in the array matches the name key for the hash.
+#if true, keep that attribute (empty hash or delete hash),
+#  if false get rid of that attribute
+
+#{"Theo"=>
+#  {:color=>
+#    [{:purple=>["Theo", "Peter Jr.", "Lucky"],
+#      :grey=>["Theo", "Peter Jr.", "Ms. K"],
+#      :white=>["Queenie", "Andrew", "Ms. K", "Alex"],
+#      :brown=>["Queenie", "Alex"]},
+#     {:purple=>["Theo", "Peter Jr.", "Lucky"],
+#      :grey=>["Theo", "Peter Jr.", "Ms. K"],
+#      :white=>["Queenie", "Andrew", "Ms. K", "Alex"],
+#      :brown=>["Queenie", "Alex"]},
+#     {:male=>["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#      :female=>["Queenie", "Ms. K"]},
